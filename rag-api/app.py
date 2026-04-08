a25adc9b0d2914aba3d0de865dbe98682e972c9c
Davi Cardoso <davicardos@ufrrj.br>
2026-04-08 15:58:31 -0300

    Ollama implementado

---
+++ rag-api/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import Ollama
+
+app = FastAPI()
+
+base = TextLoader("data/furia.txt")
+documentos = base.load()
+embeddings = HuggingFaceEmbeddings()
+db = FAISS.from_documents(documentos, embeddings)
+
+llm = Ollama(model="llama3")
+
+class Pergunta(BaseModel):
+    pergunta: str
+    
+@app.post("/pergunta")
+def responder(pergunta: Pergunta):
+    resposta = db.similarity_search(pergunta.pergunta)
+    return {"resposta": resposta[0].page_content}
