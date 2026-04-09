f4136c9a9a40da979773ada79fadde67a73f1897
Davi Cardoso <davicardos@ufrrj.br>
2026-04-09 17:56:21 -0300

    Atualizado para Llama phi 3 por conta da otimização

--- rag-api/app.py
+++ rag-api/app.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.llms import Ollama
-
+from langchain_ollama import OllamaLLM
+print("\n \n RODANDOOOOOOO \n \n")
 app = FastAPI()
 
 base = TextLoader("data/furia.txt")
@@ -12,7 +12,7 @@ documentos = base.load()
 embeddings = HuggingFaceEmbeddings()
 db = FAISS.from_documents(documentos, embeddings)
 
-llm = Ollama(model="llama3")
+llm = OllamaLLM(model="phi3")
 
 class Pergunta(BaseModel):
     pergunta: str
@@ -23,8 +23,11 @@ def responder(pergunta: Pergunta):
     contexto = resposta[0].page_content if resposta else ""
     
     prompt = f"""
-    Use o contexto abaixo para responder:
-
+    Você é um assistente especializado no time de esports FURIA.
+    Responda APENAS com base no contexto abaixo.
+    Se a resposta não estiver no contexto, diga: "Não encontrei essa informação."
+    
+    Contexto:
     {contexto}
 
     Pergunta: {pergunta.pergunta}
